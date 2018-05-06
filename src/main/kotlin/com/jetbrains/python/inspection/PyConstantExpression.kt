5377a92ef642e295b2757b42f3ec2a5a107182bb
SerVB <servbul@yandex.ru>
2018-05-06 20:11:54 +0300

    Support for parenthesized expressions; refactorings

--- src/main/kotlin/com/jetbrains/python/inspection/PyConstantExpression.kt
+++ src/main/kotlin/com/jetbrains/python/inspection/PyConstantExpression.kt
@@ -3,7 +3,6 @@ package com.jetbrains.python.inspection
 import com.intellij.codeInspection.LocalInspectionToolSession
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElementVisitor
-import com.intellij.psi.PsiElement
 import com.jetbrains.python.inspections.PyInspection
 import com.jetbrains.python.inspections.PyInspectionVisitor
 import com.jetbrains.python.PyTokenTypes
@@ -28,41 +27,64 @@ class PyConstantExpression : PyInspection() {
 
         private fun processIfPart(pyIfPart: PyIfPart) {
             val condition = pyIfPart.condition
-            when (condition) {
-                is PyBoolLiteralExpression -> handlePyBoolLiteralExpression(condition)
-                is PyBinaryExpression -> handlePyBinaryExpression(condition)
+            val result = processConstantExpression(condition) ?: return
+            registerProblem(condition, "The condition is always $result")
+        }
+
+        private fun processConstantExpression(expression: PyExpression?): Boolean? {
+            val unpackedExpression = expression.unpacked
+            return when {
+                unpackedExpression is PyBoolLiteralExpression ->
+                    handlePyBoolLiteralExpression(unpackedExpression)
+                unpackedExpression.isNumericExpression ->
+                    handlePyBinaryExpression(unpackedExpression as PyBinaryExpression)
+                else -> null
             }
         }
 
-        private fun registerBooleanProblem(element: PsiElement, value: Boolean) =
-                registerProblem(element, "The condition is always $value")
+        private fun handlePyBoolLiteralExpression(expression: PyBoolLiteralExpression) = expression.value
 
-        private fun handlePyBoolLiteralExpression(condition: PyBoolLiteralExpression) =
-                registerBooleanProblem(condition, condition.value)
-
-        private fun handlePyBinaryExpression(condition: PyBinaryExpression) {
-            val leftExpression = condition.leftExpression ?: return
-            val rightExpression = condition.rightExpression ?: return
+        private fun handlePyBinaryExpression(expression: PyBinaryExpression): Boolean? {
+            val leftExpression = expression.leftExpression.unpacked ?: return null
+            val rightExpression = expression.rightExpression.unpacked ?: return null
 
             if (leftExpression is PyNumericLiteralExpression && rightExpression is PyNumericLiteralExpression) {
                 if (leftExpression.isIntegerLiteral && rightExpression.isIntegerLiteral) {
                     /* TODO: Can leftExpression be null if it is already integer literal?
                        Maybe it is more correct to write this:
                        val leftValue = leftExpression.bigIntegerValue!! */
-                    val leftValue = leftExpression.bigIntegerValue ?: return
-                    val rightValue = rightExpression.bigIntegerValue ?: return
+                    val leftValue = leftExpression.bigIntegerValue ?: return null
+                    val rightValue = rightExpression.bigIntegerValue ?: return null
 
-                    when (condition.operator) {
-                        PyTokenTypes.LT -> registerBooleanProblem(condition, leftValue < rightValue)
-                        PyTokenTypes.LE -> registerBooleanProblem(condition, leftValue <= rightValue)
-                        PyTokenTypes.GT -> registerBooleanProblem(condition, leftValue > rightValue)
-                        PyTokenTypes.GE -> registerBooleanProblem(condition, leftValue >= rightValue)
-                        PyTokenTypes.EQEQ -> registerBooleanProblem(condition, leftValue == rightValue)
-                        PyTokenTypes.NE, PyTokenTypes.NE_OLD ->
-                            registerBooleanProblem(condition, leftValue != rightValue)
+                    return when (expression.operator) {
+                        PyTokenTypes.LT -> leftValue < rightValue
+                        PyTokenTypes.LE -> leftValue <= rightValue
+                        PyTokenTypes.GT -> leftValue > rightValue
+                        PyTokenTypes.GE -> leftValue >= rightValue
+                        PyTokenTypes.EQEQ -> leftValue == rightValue
+                        PyTokenTypes.NE, PyTokenTypes.NE_OLD -> leftValue != rightValue
+                        else -> null
                     }
                 }
             }
+            return null
         }
     }
 }
+
+val PyExpression?.unpacked: PyExpression? get() {
+    var answer = this
+    while (answer is PyParenthesizedExpression) {
+        answer = answer.containedExpression
+    }
+    return answer
+}
+
+val PyExpression?.isNumericExpression get() =
+        this is PyBinaryExpression && (this.operator == PyTokenTypes.LT ||
+                                       this.operator == PyTokenTypes.LE ||
+                                       this.operator == PyTokenTypes.GT ||
+                                       this.operator == PyTokenTypes.GE ||
+                                       this.operator == PyTokenTypes.EQEQ ||
+                                       this.operator == PyTokenTypes.NE ||
+                                       this.operator == PyTokenTypes.NE_OLD)
---
+++ tests/test5-parenthesized-expressions.py
@@ -0,0 +1,6 @@
+if (True):
+    pass
+if ((False)):
+    pass
+if (3 > 5):
+    pass
